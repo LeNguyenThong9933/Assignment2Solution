85eac44535d3936e72e19116d48ec74e194e45de
ADMIN <ADMIN@PINETREE>
2024-03-02 01:15:50 +0700

    Add project files.

---
+++ SalesWinApp/frmLogin.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace SalesWinApp
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lbEmail = new Label();
+            lbPassword = new Label();
+            txtEmail = new TextBox();
+            txtPassword = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // lbEmail
+            // 
+            lbEmail.AutoSize = true;
+            lbEmail.Location = new Point(44, 73);
+            lbEmail.Name = "lbEmail";
+            lbEmail.Size = new Size(46, 20);
+            lbEmail.TabIndex = 4;
+            lbEmail.Text = "Email";
+            // 
+            // lbPassword
+            // 
+            lbPassword.AutoSize = true;
+            lbPassword.Location = new Point(44, 127);
+            lbPassword.Name = "lbPassword";
+            lbPassword.Size = new Size(70, 20);
+            lbPassword.TabIndex = 5;
+            lbPassword.Text = "Password";
+            // 
+            // txtEmail
+            // 
+            txtEmail.Location = new Point(133, 66);
+            txtEmail.Name = "txtEmail";
+            txtEmail.Size = new Size(184, 27);
+            txtEmail.TabIndex = 6;
+            // 
+            // txtPassword
+            // 
+            txtPassword.Location = new Point(133, 124);
+            txtPassword.Name = "txtPassword";
+            txtPassword.Size = new Size(184, 27);
+            txtPassword.TabIndex = 7;
+            txtPassword.UseSystemPasswordChar = true;
+            // 
+            // btnLogin
+            // 
+            btnLogin.Location = new Point(166, 187);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(94, 29);
+            btnLogin.TabIndex = 8;
+            btnLogin.Text = "Login";
+            btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // frmLogin
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(379, 266);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPassword);
+            Controls.Add(txtEmail);
+            Controls.Add(lbPassword);
+            Controls.Add(lbEmail);
+            Name = "frmLogin";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Login";
+            Load += frmLogin_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label lbEmail;
+        private Label lbPassword;
+        private TextBox txtEmail;
+        private TextBox txtPassword;
+        private Button btnLogin;
+    }
+}
